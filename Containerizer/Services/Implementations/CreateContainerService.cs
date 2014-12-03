0f0e3042a98c2b15a1a55b5d2b28534e7ced3e86
David Morhovich + David Tengdin <pair+dmorhovich+dtengdin@pivotal.io>
2014-12-03 11:23:02 -0500

    reformat

--- Containerizer/Services/Implementations/CreateContainerService.cs
+++ Containerizer/Services/Implementations/CreateContainerService.cs
@@ -12,39 +12,39 @@ namespace Containerizer.Services.Implementations
     {
         public Task<string> CreateContainer()
         {
-                return Task.Factory.StartNew(() =>
-                {
-            try
+            return Task.Factory.StartNew(() =>
             {
-                string id = Guid.NewGuid().ToString();
-                var serverManager = ServerManager.OpenRemote("localhost");
-                string rootDir =
-                    Directory.GetDirectoryRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-                string path = Path.Combine(rootDir, "containerizer", id);
-                Site site = serverManager.Sites.Add(id, path, 0);
+                try
+                {
+                    string id = Guid.NewGuid().ToString();
+                    ServerManager serverManager = ServerManager.OpenRemote("localhost");
+                    string rootDir =
+                        Directory.GetDirectoryRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                    string path = Path.Combine(rootDir, "containerizer", id);
+                    Site site = serverManager.Sites.Add(id, path, 0);
 
-                serverManager.ApplicationPools.Add(id);
-                site.Applications[0].ApplicationPoolName = id;
-                ApplicationPool appPool = serverManager.ApplicationPools[id];
-                appPool.ManagedPipelineMode = ManagedPipelineMode.Integrated;
+                    serverManager.ApplicationPools.Add(id);
+                    site.Applications[0].ApplicationPoolName = id;
+                    ApplicationPool appPool = serverManager.ApplicationPools[id];
+                    appPool.ManagedPipelineMode = ManagedPipelineMode.Integrated;
 
                     serverManager.CommitChanges();
                     Directory.CreateDirectory(path);
                     return id;
-            }
-            catch (COMException ex)
-            {
-                if (ex.Message.Contains("2B72133B-3F5B-4602-8952-803546CE3344"))
-                {
-                    throw new CouldNotCreateContainerException("Please install IIS.", ex);
                 }
-                throw new CouldNotCreateContainerException("", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new CouldNotCreateContainerException(String.Empty, ex);
-            }
-                });
+                catch (COMException ex)
+                {
+                    if (ex.Message.Contains("2B72133B-3F5B-4602-8952-803546CE3344"))
+                    {
+                        throw new CouldNotCreateContainerException("Please install IIS.", ex);
+                    }
+                    throw new CouldNotCreateContainerException("", ex);
+                }
+                catch (Exception ex)
+                {
+                    throw new CouldNotCreateContainerException(String.Empty, ex);
+                }
+            });
         }
     }
 }
