7c88a4c080142f00984e2f9e0217046576025da3
David Varvel <pair+dvarvel@pivotal.io>
2015-02-18 15:55:38 -0500

    Add Godeps

---
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/garden/client/client_test.go
@@ -0,0 +1,191 @@
+package client_test
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/cloudfoundry-incubator/garden"
+	. "github.com/cloudfoundry-incubator/garden/client"
+	"github.com/cloudfoundry-incubator/garden/client/connection"
+	"github.com/cloudfoundry-incubator/garden/client/connection/fakes"
+)
+
+var _ = Describe("Client", func() {
+	var client Client
+
+	var fakeConnection *fakes.FakeConnection
+
+	BeforeEach(func() {
+		fakeConnection = new(fakes.FakeConnection)
+	})
+
+	JustBeforeEach(func() {
+		client = New(fakeConnection)
+	})
+
+	Describe("Capacity", func() {
+		BeforeEach(func() {
+			fakeConnection.CapacityReturns(
+				garden.Capacity{
+					MemoryInBytes: 1111,
+					DiskInBytes:   2222,
+					MaxContainers: 42,
+				},
+				nil,
+			)
+		})
+
+		It("sends a capacity request and returns the capacity", func() {
+			capacity, err := client.Capacity()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(capacity.MemoryInBytes).Should(Equal(uint64(1111)))
+			Ω(capacity.DiskInBytes).Should(Equal(uint64(2222)))
+		})
+
+		Context("when getting capacity fails", func() {
+			disaster := errors.New("oh no!")
+
+			BeforeEach(func() {
+				fakeConnection.CapacityReturns(garden.Capacity{}, disaster)
+			})
+
+			It("returns the error", func() {
+				_, err := client.Capacity()
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+	})
+
+	Describe("Create", func() {
+		It("sends a create request and returns a container", func() {
+			spec := garden.ContainerSpec{
+				RootFSPath: "/some/roofs",
+			}
+
+			fakeConnection.CreateReturns("some-handle", nil)
+
+			container, err := client.Create(spec)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(container).ShouldNot(BeNil())
+
+			Ω(fakeConnection.CreateArgsForCall(0)).Should(Equal(spec))
+
+			Ω(container.Handle()).Should(Equal("some-handle"))
+		})
+
+		Context("when there is a connection error", func() {
+			disaster := errors.New("oh no!")
+
+			BeforeEach(func() {
+				fakeConnection.CreateReturns("", disaster)
+			})
+
+			It("returns it", func() {
+				_, err := client.Create(garden.ContainerSpec{})
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+	})
+
+	Describe("Containers", func() {
+		It("sends a list request and returns all containers", func() {
+			fakeConnection.ListReturns([]string{"handle-a", "handle-b"}, nil)
+
+			props := garden.Properties{"foo": "bar"}
+
+			containers, err := client.Containers(props)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(fakeConnection.ListArgsForCall(0)).Should(Equal(props))
+
+			Ω(containers).Should(HaveLen(2))
+			Ω(containers[0].Handle()).Should(Equal("handle-a"))
+			Ω(containers[1].Handle()).Should(Equal("handle-b"))
+		})
+
+		Context("when there is a connection error", func() {
+			disaster := errors.New("oh no!")
+
+			BeforeEach(func() {
+				fakeConnection.ListReturns(nil, disaster)
+			})
+
+			It("returns it", func() {
+				_, err := client.Containers(nil)
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+	})
+
+	Describe("Destroy", func() {
+		It("sends a destroy request", func() {
+			err := client.Destroy("some-handle")
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(fakeConnection.DestroyArgsForCall(0)).Should(Equal("some-handle"))
+		})
+
+		Context("when there is a connection error", func() {
+			disaster := errors.New("oh no!")
+
+			BeforeEach(func() {
+				fakeConnection.DestroyReturns(disaster)
+			})
+
+			It("returns it", func() {
+				err := client.Destroy("some-handle")
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+
+		Context("when the error is a 404", func() {
+			notFound := connection.Error{404, ""}
+
+			BeforeEach(func() {
+				fakeConnection.DestroyReturns(notFound)
+			})
+
+			It("returns an ContainerNotFoundError with the requested handle", func() {
+				err := client.Destroy("some-handle")
+				Ω(err).Should(MatchError(garden.ContainerNotFoundError{"some-handle"}))
+			})
+		})
+	})
+
+	Describe("Lookup", func() {
+		It("sends a list request", func() {
+			fakeConnection.ListReturns([]string{"some-handle", "some-other-handle"}, nil)
+
+			container, err := client.Lookup("some-handle")
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(container.Handle()).Should(Equal("some-handle"))
+		})
+
+		Context("when the container is not found", func() {
+			BeforeEach(func() {
+				fakeConnection.ListReturns([]string{"some-other-handle"}, nil)
+			})
+
+			It("returns ContainerNotFoundError", func() {
+				_, err := client.Lookup("some-handle")
+				Ω(err).Should(MatchError(garden.ContainerNotFoundError{"some-handle"}))
+			})
+		})
+
+		Context("when there is a connection error", func() {
+			disaster := errors.New("oh no!")
+
+			BeforeEach(func() {
+				fakeConnection.ListReturns(nil, disaster)
+			})
+
+			It("returns it", func() {
+				_, err := client.Lookup("some-handle")
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+	})
+})
---
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/garden/routes/routes.go
@@ -0,0 +1,78 @@
+package routes
+
+import "github.com/tedsuo/rata"
+
+const (
+	Ping     = "Ping"
+	Capacity = "Capacity"
+
+	List    = "List"
+	Create  = "Create"
+	Info    = "Info"
+	Destroy = "Destroy"
+
+	Stop = "Stop"
+
+	StreamIn  = "StreamIn"
+	StreamOut = "StreamOut"
+
+	LimitBandwidth         = "LimitBandwidth"
+	CurrentBandwidthLimits = "CurrentBandwidthLimits"
+
+	LimitCPU         = "LimitCPU"
+	CurrentCPULimits = "CurrentCPULimits"
+
+	LimitDisk         = "LimitDisk"
+	CurrentDiskLimits = "CurrentDiskLimits"
+
+	LimitMemory         = "LimitMemory"
+	CurrentMemoryLimits = "CurrentMemoryLimits"
+
+	NetIn  = "NetIn"
+	NetOut = "NetOut"
+
+	Run    = "Run"
+	Attach = "Attach"
+
+	GetProperty    = "GetProperty"
+	SetProperty    = "SetProperty"
+	RemoveProperty = "RemoveProperty"
+)
+
+var Routes = rata.Routes{
+	{Path: "/ping", Method: "GET", Name: Ping},
+	{Path: "/capacity", Method: "GET", Name: Capacity},
+
+	{Path: "/containers", Method: "GET", Name: List},
+	{Path: "/containers", Method: "POST", Name: Create},
+
+	{Path: "/containers/:handle/info", Method: "GET", Name: Info},
+
+	{Path: "/containers/:handle", Method: "DELETE", Name: Destroy},
+	{Path: "/containers/:handle/stop", Method: "PUT", Name: Stop},
+
+	{Path: "/containers/:handle/files", Method: "PUT", Name: StreamIn},
+	{Path: "/containers/:handle/files", Method: "GET", Name: StreamOut},
+
+	{Path: "/containers/:handle/limits/bandwidth", Method: "PUT", Name: LimitBandwidth},
+	{Path: "/containers/:handle/limits/bandwidth", Method: "GET", Name: CurrentBandwidthLimits},
+
+	{Path: "/containers/:handle/limits/cpu", Method: "PUT", Name: LimitCPU},
+	{Path: "/containers/:handle/limits/cpu", Method: "GET", Name: CurrentCPULimits},
+
+	{Path: "/containers/:handle/limits/disk", Method: "PUT", Name: LimitDisk},
+	{Path: "/containers/:handle/limits/disk", Method: "GET", Name: CurrentDiskLimits},
+
+	{Path: "/containers/:handle/limits/memory", Method: "PUT", Name: LimitMemory},
+	{Path: "/containers/:handle/limits/memory", Method: "GET", Name: CurrentMemoryLimits},
+
+	{Path: "/containers/:handle/net/in", Method: "POST", Name: NetIn},
+	{Path: "/containers/:handle/net/out", Method: "POST", Name: NetOut},
+
+	{Path: "/containers/:handle/processes", Method: "POST", Name: Run},
+	{Path: "/containers/:handle/processes/:pid", Method: "GET", Name: Attach},
+
+	{Path: "/containers/:handle/properties/:key", Method: "GET", Name: GetProperty},
+	{Path: "/containers/:handle/properties/:key", Method: "PUT", Name: SetProperty},
+	{Path: "/containers/:handle/properties/:key", Method: "DELETE", Name: RemoveProperty},
+}
