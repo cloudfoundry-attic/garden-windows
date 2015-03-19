c57c8190f11414de992b6b28bba954120c76d398
David Morhovich <pair+dmorhovich@pivotal.io>
2015-03-19 11:23:14 -0400

    Restructures containerizer project into a console application.
    Restructures containerizer project into a console application, converted
    from an IIS web application project.

--- Containerizer/Properties/AssemblyInfo.cs
+++ Containerizer/Properties/AssemblyInfo.cs
@@ -1,32 +1,26 @@
-﻿#region
-
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("Containerizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Containerizer")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("15c57bd4-a2eb-48c8-89bb-acd56faa7d28")]
+[assembly: Guid("76abf013-7dc1-40b7-b8b8-c97422d43c32")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
