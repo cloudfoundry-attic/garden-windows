533a3863c231e9112c8364b0ffd20abc9faba456
David Morhovich & David Goddard <pair+dgoddarg+dmorhovich@pivotal.io>
2015-03-23 10:06:08 -0400

    Add make script [#90818444]

---
+++ make.bat
@@ -0,0 +1,18 @@
+:: Visual Studio must be in path
+
+where devenv
+if errorLevel 1 ( echo "devenv was not found on PATH" && exit /b 1 )
+ 
+:: enable some features
+SET dism=%WINDIR%\SysNative\dism.exe
+%dism% /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart
+%dism% /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart
+%dism% /online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart
+%dism% /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart
+
+del /F /Q packages\*
+bin\nuget restore || exit /b 1
+devenv Containerizer\Containerizer.csproj /build "Release" || exit /b 1
+devenv Containerizer.Tests\Containerizer.Tests.csproj /build "Release" || exit /b 1
+packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Release\Containerizer.Tests.dll || exit /b 1
+
