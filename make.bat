da3e52006e003f707558f877aa2920453d5ecf1c
Dave Goddard <dgoddard@pivotal.io>
2015-03-23 18:47:19 +0000

    Update make.bat
    Add path for MSBuild, and use dism directly instead of through SysNative directory
    
    This should make to run on generic windows 2012 (x64) systems.
    
    [#90928968]

--- make.bat
+++ make.bat
@@ -1,13 +1,12 @@
-:: enable some features
-SET dism=%WINDIR%\SysNative\dism.exe
-%dism% /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart
-%dism% /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart
-%dism% /online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart
-%dism% /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart
+SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319
+
+dism /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart || exit /b 1
+dism /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart || exit /b 1
+dism /online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart || exit /b 1
+dism /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart || exit /b 1
 
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
 MSBuild Containerizer\Containerizer.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
 MSBuild Containerizer.Tests\Containerizer.Tests.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Release\Containerizer.Tests.dll || exit /b 1
-
