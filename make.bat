262114133a2a6990f07d93a8523b2f7c94f868d0
David Morhovich & David Goddard <pair+dgoddarg+dmorhovich@pivotal.io>
2015-03-23 10:53:07 -0400

    Use msbuild instead of devenv [#90905976]

--- make.bat
+++ make.bat
@@ -1,8 +1,3 @@
-:: Visual Studio must be in path
-
-where devenv
-if errorLevel 1 ( echo "devenv was not found on PATH" && exit /b 1 )
- 
 :: enable some features
 SET dism=%WINDIR%\SysNative\dism.exe
 %dism% /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart
@@ -12,7 +7,7 @@ SET dism=%WINDIR%\SysNative\dism.exe
 
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
-devenv Containerizer\Containerizer.csproj /build "Release" || exit /b 1
-devenv Containerizer.Tests\Containerizer.Tests.csproj /build "Release" || exit /b 1
+MSBuild Containerizer\Containerizer.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
+MSBuild Containerizer.Tests\Containerizer.Tests.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Release\Containerizer.Tests.dll || exit /b 1
 
