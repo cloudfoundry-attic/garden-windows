f7f54e5901e537e1b505d91a70a4db429c134539
John Shahid & David Morhovich <pair+jshahid+dmorhovich@pivotal.io>
2015-03-25 10:08:15 -0400

    don't turn on features since containerizer doesn't depend on them

--- make.bat
+++ make.bat
@@ -3,11 +3,6 @@ SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319
 where msbuild
 if errorLevel 1 ( echo "msbuild was not found on PATH" && exit /b 1 )
 
-dism /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart || exit /b 1
-dism /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart || exit /b 1
-dism /online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart || exit /b 1
-dism /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart || exit /b 1
-
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
 MSBuild Containerizer\Containerizer.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
