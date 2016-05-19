8ab0f4bb3557c4c6b59dac1bf7b149cff56b8d41
Matthew Horan <mhoran@pivotal.io>
2016-05-19 12:07:33 -0400

    Enable running NSpec tests via the ReSharper test runner
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- Containerizer/Containerizer.Tests/DebuggerShim.cs
+++ Containerizer/Containerizer.Tests/DebuggerShim.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
 using System.Reflection;
 using NSpec;
 using NSpec.Domain;
 using NSpec.Domain.Formatters;
+using NUnit.Framework;
 
 /*
  * Howdy,
@@ -37,6 +39,23 @@ public class DebuggerShim
         var results = runner.Run(builder.Contexts().Build());
 
         //assert that there aren't any failures
+        results.Failures().Count().should_be(0);
+    }
+
+
+}
+
+[TestFixture]
+public abstract class nspec : global::NSpec.nspec
+{
+    [Test]
+    public void debug()
+    {
+        var currentSpec = this.GetType();
+        var finder = new SpecFinder(new[] {currentSpec});
+        var builder = new ContextBuilder(finder,new Tags().Parse(currentSpec.Name),new DefaultConventions());
+        var runner = new ContextRunner(builder,new ConsoleFormatter(), false);
+        var results = runner.Run(builder.Contexts().Build());
         results.Failures().Count().should_be(0);
     }
 }
