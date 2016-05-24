38cb2c7a1d6da36148232a8b591cf3fdf7c1736b
Amin Jamali <ajamali@pivotal.io>
2016-05-24 15:23:22 -0400

    Fix make.bat
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- Containerizer/make.bat
+++ Containerizer/make.bat
@@ -2,7 +2,7 @@
 SET DEVENV_PATH=%programfiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE
 SET PATH=%PATH%;%DEVENV_PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319
 where msbuild
-if errorLevel 1 ( echo "msbuild was not found on PATH" && exit /b 1 )
+if %errorLevel% NEQ 0 ( echo "msbuild was not found on PATH" && exit /b 1 )
 
 pushd IronFrame || exit /b 1
   ..\bin\nuget restore || exit /b 1
@@ -17,4 +17,4 @@ MSBuild Containerizer\Containerizer.csproj /t:Rebuild /p:Configuration=Release |
 MSBuild Containerizer.Tests\Containerizer.Tests.csproj /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Containerizer.Tests\bin\Release\Containerizer.Tests.dll || exit /b 1
 
-robocopy Containerizer\bin\ ..\output\
+(robocopy Containerizer\bin\ ..\output\) ^& IF %ERRORLEVEL% LEQ 1 exit /b 0 
